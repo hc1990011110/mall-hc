a9373622c951ccd931e5e1ff8751e86829e7cc5a
huangchao <332330633@qq.com>
2024-10-24 20:34:59 +0800

    docker.host

---
+++ mall-backend/src/test/java/com/hc/mall/BackendTest.java
@@ -0,0 +1,19 @@
+package com.hc.mall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+//@SpringBootTest
+public class BackendTest {
+
+    @Test
+    public void test1(){
+        log.warn("BackendTest！");
+    }
+
+    @Test
+    public void test2(){
+        log.warn("fuckyou！");
+    }
+}
---
+++ mall-frontend/src/test/java/com/hc/mall/FrontendTest.java
@@ -0,0 +1,14 @@
+package com.hc.mall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+//@SpringBootTest
+public class FrontendTest {
+
+    @Test
+    public void test1(){
+        log.warn("FrontendTest！");
+    }
+}
