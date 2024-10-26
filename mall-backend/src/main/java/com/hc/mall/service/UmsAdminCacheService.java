b893ed44e5e656954c493e475757f304729f60b0
huangchao <332330633@qq.com>
2024-10-26 18:14:34 +0800

    后台用户相关

--- mall-backend/src/main/java/com/hc/mall/service/UmsAdminCacheService.java
+++ mall-backend/src/main/java/com/hc/mall/service/UmsAdminCacheService.java
@@ -1,24 +1,24 @@
-//package com.hc.mall.service;
-//
-//import com.hc.mall.model.UmsAdmin;
-//
-///**
-// * 后台用户缓存操作类
-// * Created by hc on 2020/3/13.
-// */
-//public interface UmsAdminCacheService {
-//    /**
-//     * 删除后台用户缓存
-//     */
-//    void delAdmin(Long adminId);
-//
-//    /**
-//     * 获取缓存后台用户信息
-//     */
-//    UmsAdmin getAdmin(Long adminId);
-//
-//    /**
-//     * 设置缓存后台用户信息
-//     */
-//    void setAdmin(UmsAdmin admin);
-//}
+package com.hc.mall.service;
+
+import com.hc.mall.model.UmsAdmin;
+
+/**
+ * 后台用户缓存操作类
+ * Created by hc on 2020/3/13.
+ */
+public interface UmsAdminCacheService {
+    /**
+     * 删除后台用户缓存
+     */
+    void delAdmin(Long adminId);
+
+    /**
+     * 获取缓存后台用户信息
+     */
+    UmsAdmin getAdmin(Long adminId);
+
+    /**
+     * 设置缓存后台用户信息
+     */
+    void setAdmin(UmsAdmin admin);
+}
--- mall-backend/src/main/java/com/hc/mall/service/impl/UmsAdminCacheServiceImpl.java
+++ mall-backend/src/main/java/com/hc/mall/service/impl/UmsAdminCacheServiceImpl.java
@@ -1,42 +1,42 @@
-//package com.hc.mall.service.impl;
-//
-//import com.hc.mall.common.service.RedisService;
-//import com.hc.mall.model.UmsAdmin;
-//import com.hc.mall.service.UmsAdminCacheService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * UmsAdminCacheService实现类
-// * Created by hc on 2020/3/13.
-// */
-//@Service
-//public class UmsAdminCacheServiceImpl implements UmsAdminCacheService {
-//    @Autowired
-//    private RedisService redisService;
-//    @Value("${redis.database}")
-//    private String REDIS_DATABASE;
-//    @Value("${redis.expire.common}")
-//    private Long REDIS_EXPIRE;
-//    @Value("${redis.key.admin}")
-//    private String REDIS_KEY_ADMIN;
-//
-//    @Override
-//    public void delAdmin(Long adminId) {
-//        String key = REDIS_DATABASE + ":" + REDIS_KEY_ADMIN + ":" + adminId;
-//        redisService.del(key);
-//    }
-//
-//    @Override
-//    public UmsAdmin getAdmin(Long adminId) {
-//        String key = REDIS_DATABASE + ":" + REDIS_KEY_ADMIN + ":" + adminId;
-//        return (UmsAdmin) redisService.get(key);
-//    }
-//
-//    @Override
-//    public void setAdmin(UmsAdmin admin) {
-//        String key = REDIS_DATABASE + ":" + REDIS_KEY_ADMIN + ":" + admin.getId();
-//        redisService.set(key, admin, REDIS_EXPIRE);
-//    }
-//}
+package com.hc.mall.service.impl;
+
+import com.hc.mall.common.service.RedisService;
+import com.hc.mall.model.UmsAdmin;
+import com.hc.mall.service.UmsAdminCacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * UmsAdminCacheService实现类
+ * Created by hc on 2020/3/13.
+ */
+@Service
+public class UmsAdminCacheServiceImpl implements UmsAdminCacheService {
+    @Autowired
+    private RedisService redisService;
+    @Value("${redis.database}")
+    private String REDIS_DATABASE;
+    @Value("${redis.expire.common}")
+    private Long REDIS_EXPIRE;
+    @Value("${redis.key.admin}")
+    private String REDIS_KEY_ADMIN;
+
+    @Override
+    public void delAdmin(Long adminId) {
+        String key = REDIS_DATABASE + ":" + REDIS_KEY_ADMIN + ":" + adminId;
+        redisService.del(key);
+    }
+
+    @Override
+    public UmsAdmin getAdmin(Long adminId) {
+        String key = REDIS_DATABASE + ":" + REDIS_KEY_ADMIN + ":" + adminId;
+        return (UmsAdmin) redisService.get(key);
+    }
+
+    @Override
+    public void setAdmin(UmsAdmin admin) {
+        String key = REDIS_DATABASE + ":" + REDIS_KEY_ADMIN + ":" + admin.getId();
+        redisService.set(key, admin, REDIS_EXPIRE);
+    }
+}
